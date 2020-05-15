8378e32d2b1b205e502946d3a3eadada9f9e986b
Daniel Sottile <danielsottile@gmail.com>
2020-05-15 13:07:45 -0400

    first part of server side complete

--- src/middleware/auth-service.js
+++ src/middleware/auth-service.js
@@ -1,17 +1,29 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
 const AuthService = {
-    getUserWithUserName(db, user_name) {
-        return db('thingful_users')
-            .where({user_name})
-            .first()
-},
+  getUserWithUserName(db, user_name) {
+    return db('thingful_users')
+      .where({user_name})
+      .first();
+  },
 
-parseBasicToken(token){
+  comparePasswords(password, hash) {
+    return bcrypt.compare(password, hash);
+  },
+
+  createJwt(subject, payload) {
+    return jwt.sign(payload, config.JWT_SECRET, {subject});
+  },
+
+  parseBasicToken(token){
     return Buffer
-        .from(token, 'base64')
-        .toString()
-        .split(':')
- },
-}
+      .from(token, 'base64')
+      .toString()
+      .split(':');
+  },
+};
 
 
-module.exports = AuthService
+module.exports = AuthService;
